0a790ecf940f69e0e4aa19cc0e15ea7110804f61
Mark <6marklouie9@gmail.com>
2026-07-16 17:11:13 +0800

    Add files via upload

---
+++ Mashalla/Mashalla/Form2.Designer.vb
@@ -0,0 +1,304 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form2))
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.tabObjects = New System.Windows.Forms.TabPage()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.tabProperties = New System.Windows.Forms.TabPage()
+        Me.BackButton = New System.Windows.Forms.Button()
+        Me.LabelShape = New System.Windows.Forms.Label()
+        Me.LabelMaterial = New System.Windows.Forms.Label()
+        Me.LabelColor = New System.Windows.Forms.Label()
+        Me.LabelObject = New System.Windows.Forms.Label()
+        Me.TabControl1.SuspendLayout()
+        Me.tabObjects.SuspendLayout()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.tabProperties.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.tabObjects)
+        Me.TabControl1.Controls.Add(Me.tabProperties)
+        Me.TabControl1.Location = New System.Drawing.Point(4, 1)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(966, 515)
+        Me.TabControl1.TabIndex = 0
+        '
+        'tabObjects
+        '
+        Me.tabObjects.Controls.Add(Me.Button5)
+        Me.tabObjects.Controls.Add(Me.Button4)
+        Me.tabObjects.Controls.Add(Me.Button3)
+        Me.tabObjects.Controls.Add(Me.Button2)
+        Me.tabObjects.Controls.Add(Me.Button1)
+        Me.tabObjects.Controls.Add(Me.PictureBox5)
+        Me.tabObjects.Controls.Add(Me.PictureBox4)
+        Me.tabObjects.Controls.Add(Me.PictureBox3)
+        Me.tabObjects.Controls.Add(Me.PictureBox2)
+        Me.tabObjects.Controls.Add(Me.PictureBox1)
+        Me.tabObjects.Location = New System.Drawing.Point(4, 22)
+        Me.tabObjects.Name = "tabObjects"
+        Me.tabObjects.Padding = New System.Windows.Forms.Padding(3)
+        Me.tabObjects.Size = New System.Drawing.Size(958, 489)
+        Me.tabObjects.TabIndex = 0
+        Me.tabObjects.Text = "Objects"
+        Me.tabObjects.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.Transparent
+        Me.Button5.BackgroundImage = CType(resources.GetObject("Button5.BackgroundImage"), System.Drawing.Image)
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(734, 300)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(161, 25)
+        Me.Button5.TabIndex = 9
+        Me.Button5.Text = "See Properties"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.Transparent
+        Me.Button4.BackgroundImage = CType(resources.GetObject("Button4.BackgroundImage"), System.Drawing.Image)
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(562, 300)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(161, 25)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "See Properties"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.Transparent
+        Me.Button3.BackgroundImage = CType(resources.GetObject("Button3.BackgroundImage"), System.Drawing.Image)
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(390, 300)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(161, 25)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "See Properties"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Transparent
+        Me.Button2.BackgroundImage = CType(resources.GetObject("Button2.BackgroundImage"), System.Drawing.Image)
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(218, 300)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(161, 25)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "See Properties"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Transparent
+        Me.Button1.BackgroundImage = CType(resources.GetObject("Button1.BackgroundImage"), System.Drawing.Image)
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(36, 300)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(176, 25)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "See Properties"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox5.Image = CType(resources.GetObject("PictureBox5.Image"), System.Drawing.Image)
+        Me.PictureBox5.Location = New System.Drawing.Point(729, 155)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(166, 135)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox5.TabIndex = 4
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox4.Image = CType(resources.GetObject("PictureBox4.Image"), System.Drawing.Image)
+        Me.PictureBox4.Location = New System.Drawing.Point(557, 155)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(166, 135)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox4.TabIndex = 3
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(385, 155)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(166, 135)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox3.TabIndex = 2
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(213, 155)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(166, 135)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(41, 155)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(166, 135)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'tabProperties
+        '
+        Me.tabProperties.Controls.Add(Me.BackButton)
+        Me.tabProperties.Controls.Add(Me.LabelShape)
+        Me.tabProperties.Controls.Add(Me.LabelMaterial)
+        Me.tabProperties.Controls.Add(Me.LabelColor)
+        Me.tabProperties.Controls.Add(Me.LabelObject)
+        Me.tabProperties.Location = New System.Drawing.Point(4, 22)
+        Me.tabProperties.Name = "tabProperties"
+        Me.tabProperties.Padding = New System.Windows.Forms.Padding(3)
+        Me.tabProperties.Size = New System.Drawing.Size(958, 489)
+        Me.tabProperties.TabIndex = 1
+        Me.tabProperties.Text = "Properties"
+        Me.tabProperties.UseVisualStyleBackColor = True
+        '
+        'BackButton
+        '
+        Me.BackButton.BackColor = System.Drawing.Color.Transparent
+        Me.BackButton.BackgroundImage = CType(resources.GetObject("BackButton.BackgroundImage"), System.Drawing.Image)
+        Me.BackButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BackButton.Location = New System.Drawing.Point(87, 290)
+        Me.BackButton.Name = "BackButton"
+        Me.BackButton.Size = New System.Drawing.Size(107, 45)
+        Me.BackButton.TabIndex = 6
+        Me.BackButton.Text = "Back"
+        Me.BackButton.UseVisualStyleBackColor = False
+        '
+        'LabelShape
+        '
+        Me.LabelShape.AutoSize = True
+        Me.LabelShape.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelShape.Location = New System.Drawing.Point(83, 232)
+        Me.LabelShape.Name = "LabelShape"
+        Me.LabelShape.Size = New System.Drawing.Size(70, 24)
+        Me.LabelShape.TabIndex = 3
+        Me.LabelShape.Text = "Shape"
+        '
+        'LabelMaterial
+        '
+        Me.LabelMaterial.AutoSize = True
+        Me.LabelMaterial.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelMaterial.Location = New System.Drawing.Point(83, 204)
+        Me.LabelMaterial.Name = "LabelMaterial"
+        Me.LabelMaterial.Size = New System.Drawing.Size(83, 24)
+        Me.LabelMaterial.TabIndex = 2
+        Me.LabelMaterial.Text = "Material"
+        '
+        'LabelColor
+        '
+        Me.LabelColor.AutoSize = True
+        Me.LabelColor.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelColor.Location = New System.Drawing.Point(83, 176)
+        Me.LabelColor.Name = "LabelColor"
+        Me.LabelColor.Size = New System.Drawing.Size(60, 24)
+        Me.LabelColor.TabIndex = 1
+        Me.LabelColor.Text = "Color"
+        '
+        'LabelObject
+        '
+        Me.LabelObject.AutoSize = True
+        Me.LabelObject.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelObject.Location = New System.Drawing.Point(83, 117)
+        Me.LabelObject.Name = "LabelObject"
+        Me.LabelObject.Size = New System.Drawing.Size(111, 24)
+        Me.LabelObject.TabIndex = 0
+        Me.LabelObject.Text = "Properties:"
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(970, 514)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        Me.TabControl1.ResumeLayout(False)
+        Me.tabObjects.ResumeLayout(False)
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.tabProperties.ResumeLayout(False)
+        Me.tabProperties.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents tabObjects As System.Windows.Forms.TabPage
+    Friend WithEvents tabProperties As System.Windows.Forms.TabPage
+    Protected Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox5 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox4 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents LabelObject As System.Windows.Forms.Label
+    Friend WithEvents LabelShape As System.Windows.Forms.Label
+    Friend WithEvents LabelMaterial As System.Windows.Forms.Label
+    Friend WithEvents LabelColor As System.Windows.Forms.Label
+    Friend WithEvents BackButton As System.Windows.Forms.Button
+End Class
---
+++ Mashalla/Mashalla/Form2.vb
@@ -0,0 +1,62 @@
+﻿Public Class Form2
+    Private Sub ShowProperties(ByVal objName As String)
+
+        LabelObject.Text = "Object: " & objName
+
+        Select Case objName
+
+            Case "Aircon"
+                LabelColor.Text = "Color: White"
+                LabelMaterial.Text = "Material: Plastic and Metal"
+                LabelShape.Text = "Shape: Rectangular"
+
+            Case "Chair"
+                LabelColor.Text = "Color: Light Brown"
+                LabelMaterial.Text = "Material: Wood"
+                LabelShape.Text = "Shape: Square and Cylindrical"
+
+            Case "Lightbulb"
+                LabelColor.Text = "Color: Transparent"
+                LabelMaterial.Text = "Material: Glass and Metal"
+                LabelShape.Text = "Shape: Speherical"
+
+            Case "Ethernet Cable"
+                LabelColor.Text = "Color: Variety"
+                LabelMaterial.Text = "Material: Rubber and Copper"
+                LabelShape.Text = "Shape: Square and Tubular"
+
+            Case "Whiteboard"
+                LabelColor.Text = "Color: White"
+                LabelMaterial.Text = "Material: Plastic"
+                LabelShape.Text = "Shape: Rectangular"
+        End Select
+
+        TabControl1.SelectedIndex = 1
+
+    End Sub
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        ShowProperties("Aircon")
+
+    End Sub
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        ShowProperties("Chair")
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        ShowProperties("Lightbulb")
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        ShowProperties("Ethernet Cable")
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        ShowProperties("Whiteboard")
+    End Sub
+
+    Private Sub BackButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BackButton.Click
+        TabControl1.SelectedIndex = 0
+    End Sub
+End Class
+
+
